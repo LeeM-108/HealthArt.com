d06b07b10cd8b4650aba4e9edc2fd4ac2e7ee7bb
quanghuy25112000 <quanghuy2kls@gmail.com>
2020-11-01 05:19:25 +0700

    tinhs bmi

--- tinh_BMI.js
+++ tinh_BMI.js
@@ -2,8 +2,14 @@ let form=document.getElementById('myForm')
 form.onsubmit = (e) =>{
     e.preventDefault();
     let c=Number(form.b.value)/Math.pow((Number(form.a.value)/100),2)
+    c= Math.round(c * 100) / 100
     if(c<18.5){
-        window.location.href = `../baitap/giamcan.html?bmi=${c}`
+        window.location.href = `../bmi_nguoigay/nguoitangcan.html?bmi=${c}`
 
     }
+    else if(c>=25){
+        window.location.href=`../bmi_nguoibeo/nguoithuacan.html?bmi=${c}`
+    }
+    else 
+        window.location.href=`../bmi_nguoigiucan/nguoigiucan.html?bmi=${c}`
 }
